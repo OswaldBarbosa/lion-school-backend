526d40ab58bf6d2d4e1824d689745853762afcff
Oswaldo Barbosa <oswaldo.barbosacruz1902@gmail.com>
2023-03-31 15:15:27 -0300

    imagens

--- modulo/cursos.js
+++ modulo/cursos.js
@@ -2,13 +2,13 @@ var cursos = [
     {
         "nome"  :   "001 - Técnico em Desenvolvimento de Sistemas",
         "sigla" :   "DS",
-        "icone" :   "./img/redes.png",
+        "icone" :   "img/redes.png",
         "carga" :   "1200"
     },
     {
         "nome"  :   "002 - Técnico em Redes de Computadores",
         "sigla" :   "RDS",
-        "icone" :   "./img/redes.png",
+        "icone" :   "img/redes.png",
         "carga" :   "1200"
     }
 ];
