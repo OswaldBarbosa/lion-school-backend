e2db32a0b624382045bce3ed3fe2916040fe57eb
OswaldBarbosa <oswaldo.barbosacruz1902@gmail.com>
2023-04-09 22:02:16 -0300

    testando

--- modulo/functions.js
+++ modulo/functions.js
@@ -131,8 +131,6 @@ const getAlunosPorStatus = function (statusAluno) {
                 jsonAluno.sexo = aluno.sexo,
                 jsonAluno.matricula = aluno.matricula,
                 jsonAluno.status = aluno.status
-                jsonAluno.curso = lista.curso[0].nome,
-                jsonAluno.dataConclusao = lista.curso[0].conclusao;
                 novoArray.push(jsonAluno)
         }
        
