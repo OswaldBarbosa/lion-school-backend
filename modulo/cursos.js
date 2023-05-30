4b4e0b5ed34ee27317593d4bd874205948f565b8
OswldBarbosa <oswaldo.barbosacruz1902@gmail.com>
2023-05-30 16:30:16 -0300

    projeto completo

--- modulo/cursos.js
+++ modulo/cursos.js
@@ -3,13 +3,16 @@ var cursos = [
         "nome"  :   "001 - Técnico em Desenvolvimento de Sistemas",
         "sigla" :   "DS",
         "icone" :   "https://cdn-icons-png.flaticon.com/512/59/59118.png",
-        "carga" :   "1200"
+        "carga" :   "1200",
+        "descricao" :   "O curso de desenvolvimento de sistemas abrange uma ampla variedade de capacidades e habilidades relacionadas à criação e manutenção de software."
     },
     {
         "nome"  :   "002 - Técnico em Redes de Computadores",
         "sigla" :   "RDS",
         "icone" :   "https://cdn-icons-png.flaticon.com/512/182/182385.png",
-        "carga" :   "1200"
+        "carga" :   "1200",
+        "descricao" :   "Rede de computadores ou redes de dados, na informática e na telecomunicação, é um conjunto de dois ou mais dispositivos eletrônicos de computação interligados por um sistema de comunicação digital. Essa rede é guiada por um conjunto de regras que permite o compartilhamento de informação, serviços e recursos físicos e lógicos entre os dispositivos."
+
     }
 ];
 
